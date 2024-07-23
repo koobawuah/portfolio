82241913fffcffea38246939a5517f53b5944e9b
koobawuah <me@bawuahboakye.com>
2024-07-23 01:20:40 +0000

    feat: About me pop up | Expandable card

--- app/components/profile.tsx
+++ app/components/profile.tsx
@@ -1,6 +1,7 @@
 import { Link } from "@remix-run/react";
 import { motion } from "framer-motion";
 import { FaGithub, FaInstagram, FaLinkedin, FaXTwitter } from "react-icons/fa6";
+import { ExpandableCard } from "./ui/expandable-card";
 
 export default function Profile() {
   return (
@@ -25,9 +26,61 @@ export default function Profile() {
             quality products, professional work and outstanding services that
             satisfy clients.
           </em>{" "}
-          <Link to="#" className="underline text-zinc-500 hover:text-zinc-200">
-            Read more
-          </Link>
+          <ExpandableCard
+            triggerExpandableCardText="Read more"
+            title="Kofi Bawuah Boakye"
+            description="Freelance I.T Contractor"
+            src="/profile_picture.JPG"
+            ctaLink="https://cal.com/bawuahboakye"
+            ctaText="Let's chat now"
+            content={() => (
+              <>
+                <p className="text-lg mt-3 font-semibold text-zinc-500 dark:text-zinc-400 ">
+                  Growing up in Accra, Ghana, life as I experienced, was
+                  enjoyable and adventurous, filled with traveling and fun times
+                  with my family. I always had a knack for drawing cool stuff.
+                  From a young age, I displayed a talent for artistry and
+                  mechanics, often found sketching portraits and messing around
+                  with gadgets - my imagination knew no bounds!
+                  <br />
+                  <br />
+                  But things got real when life took a transformative turn as I
+                  navigated through growing up, encountering profound
+                  experiences that shaped my perspective. I discovered coding at
+                  11 and ever since then, I have been on the path of computers
+                  and the internet. I built my first website at 13. Even though
+                  I dreamed of being a mechanical engineer, fate had other
+                  plans.
+                  <br />
+                  <br />
+                  Despite initially aspiring to become a mechanical engineer,
+                  High school wasn't exactly a walk in the park, my journey led
+                  me down a different path. While attending Achimota Senior High
+                  School, I studied general arts which was quite the opposite of
+                  computers but I co-founded the first-ever ICT club, which was
+                  pretty cool. After struggling with my final exams, I finally
+                  made it to college to study IT at which point I had already
+                  been working in IT for 3 years.
+                  <br />
+                  <br />
+                  Between classes, I gained experience as a freelancer, doing
+                  graphic design and building websites. I even interned at an
+                  accounting software company, further honing my skills.
+                  Throughout my journey, I embraced opportunities for growth and
+                  seized the chance to travel once more. As I transitioned into
+                  the professional world, I worked in the University of Ghana's
+                  computer science department during my National Service.
+                  <br />
+                  <br />
+                  Now, with 8 years of experience under my belt, I continue to
+                  innovate and inspire, leveraging my diverse background to make
+                  meaningful contributions to the digital landscape in Ghana and
+                  beyond. From coding to designing, I'm living my passion and
+                  loving every minute of it!
+                </p>
+              </>
+            )}
+          />
         </p>
       </div>
       <div className="">
