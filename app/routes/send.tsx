982702c7784b6202f212df08b534df5bca52579e
koobawuah <me@bawuahboakye.com>
2024-08-02 19:11:38 +0000

    feat: formdata to server, server to notifications

---
+++ app/routes/send.tsx
@@ -0,0 +1,28 @@
+import { ActionFunctionArgs, json } from "@remix-run/node";
+import https from "https";
+
+export async function action({ request }: ActionFunctionArgs) {
+  const form = await request.formData();
+  const { message } = Object.fromEntries(form);
+
+  const sendNotification = {
+    credentials: process.env.NOTIFI_CRED,
+    title: "New message from website",
+    message: message,
+    link: "https://notifi.it/",
+    image:
+      "https://portfolio-nine-coral-28.vercel.app/favicons/favicon-32x32.png",
+  };
+
+  console.log("(received)Server says " + message);
+
+  try {
+    https.get(
+      `https://notifi.it/api?credentials=${process.env.NOTIFI_CRED}&title=${sendNotification.title}&message=${message}&link=${sendNotification.link}&image=${sendNotification.image}`
+    );
+    return json({ status: 200 });
+  } catch (e) {
+    console.error("Sorry, something went wrong: " + e);
+    throw new Error("Unable to send message: " + e);
+  }
+}
